e90a2c16ad45b096a67e3d5d0696803d293b28e8
IvanKustarev <ivan.kustarev@gmail.com>
2022-01-17 00:57:04 +0300

    create logs

--- src/main/java/web4/back/Web4BackApplication.java
+++ src/main/java/web4/back/Web4BackApplication.java
@@ -1,12 +1,16 @@
 package web4.back;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Web4BackApplication {
 
+    private static final Logger log = Logger.getLogger(Web4BackApplication.class);
+
     public static void main(String[] args) {
+        log.info("=====================Starting Spring Boot Application===============================");
         SpringApplication.run(Web4BackApplication.class, args);
     }
 
---
+++ src/main/java/web4/back/profiling/ProfilingBeanPostProcessor.java
@@ -0,0 +1,36 @@
+package web4.back.profiling;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Date;
+
+@Component
+public class ProfilingBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean.getClass().getDeclaredAnnotation(Profile.class) != null){
+            return Proxy.newProxyInstance(bean.getClass().getClassLoader(), bean.getClass().getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    Date date = new Date();
+                    Object result = method.invoke(bean, args);
+                    Long executeTime = new Date().getTime() - date.getTime();
+                    System.out.println(method.getName() + " execute for " + executeTime);
+                    return result;
+                }
+            });
+        }else {
+            return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+        }
+    }
+}
