fc501a07f2d2adec652ec3df3a6963d63ca23ab7
IvanKustarev <ivan.kustarev@gmail.com>
2022-01-17 14:04:11 +0300

    delete profiling packed because of have packed profile

--- src/main/java/web4/back/profiling/Profile.java
+++
@@ -1,8 +0,0 @@
-package web4.back.profiling;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Profile {
-}
